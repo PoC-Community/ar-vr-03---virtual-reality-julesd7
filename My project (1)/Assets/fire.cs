60057ce519fd9f75ded575672785950249ec5f38
Jules Dufraiche <jules.dufraiche@gmail.com>
2023-02-02 16:32:37 +0100

    feat: add my project

---
+++ My project (1)/Assets/fire.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class fire : MonoBehaviour
+{
+    public GameObject Bullet;
+    public Transform SpawnPoint;
+    public float BulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GameObject.Instantiate(Bullet, touches[0].pose.position, touches[0].pose. rotation);
+    }
+}
